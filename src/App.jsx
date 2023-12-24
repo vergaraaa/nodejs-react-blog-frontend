10d60dcb86c63e75651ba386a083f6b867df672f
Edgar Vergara <edgarvm13@hotmail.com>
2023-12-24 12:14:39 +0100

    feat: init react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import { Router } from './routing/router'
 
 function App() {
 
   return (
     <>
       <h1>Blog with React</h1>
+
+      <Router />
     </>
   )
 }
---
+++ src/routing/router.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { Home } from "../components/pages/Home";
+import { Articles } from "../components/pages/Articles";
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            {/* LAYOUT */}
+
+
+            {/* ROUTES */}
+            <section id="content" className="content">
+                <Routes>
+                    <Route path="/" element={<Home />} /> 
+                    <Route path="/home" element={<Home />} /> 
+                    <Route path="/articles" element={<Articles />} /> 
+                </Routes>
+            </section>
+        </BrowserRouter>
+    );
+}
