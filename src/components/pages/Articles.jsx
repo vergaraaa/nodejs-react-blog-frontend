fd8a0b576c67188ce85355dd1cb5df0a9a7fc670
Edgar Vergara <edgarvm13@hotmail.com>
2023-12-26 14:33:41 +0100

    feat: articles section layout

--- src/components/pages/Articles.jsx
+++ src/components/pages/Articles.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export const Articles = () => {
   return (
-    <div>Articles</div>
+    <>
+      <article className='article-item'>
+        <div className='mask'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
+        </div>
+
+        <div className="data">
+          <h3 className="title">Title</h3>
+          <p className="description">Description</p>
+
+          <button className="edit">Edit</button>
+          <button className="delete">Delete</button>
+        </div>
+      </article>
+    </>
   )
 }
