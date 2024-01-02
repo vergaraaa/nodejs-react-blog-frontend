6247e279344cfeffe90f2bfa889284c32458ea33
Edgar Vergara <edgarvm13@hotmail.com>
2024-01-02 11:40:59 +0100

    feat: show search results

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
 
+import { useState, useEffect } from 'react'
+import { Global } from '../../helpers/Global';
+import { Request } from '../../helpers/Request';
+import { List } from '../layouts/List';
+import { useParams } from 'react-router-dom';
+
 export const Search = () => {
+
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    getArticles();
+  }, [params]);
+
+
+  const getArticles = async () => {
+    const { query } = params;
+
+    const url = Global.url + "/articles/search/" + query;
+    const { data } = await Request(url, "GET");
+
+    setArticles(data);
+    setLoading(false);
+  }
+
   return (
-    <div>Search</div>
+    <>
+      {
+        loading
+          ? "Loading..." :
+          articles.length >= 1
+            ? <List
+              articles={articles}
+              setArticles={setArticles}
+            />
+            : <h1>No articles</h1>
+      }
+    </>
   )
 }
